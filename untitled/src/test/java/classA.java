408496a70e1b6466bcf8e4aaf2d3708bddbf35b9
Vikas Pawar <vipawar9075@gmail.com>
2024-08-24 17:21:09 +0530

    formatting done

--- untitled/src/test/java/classA.java
+++ untitled/src/test/java/classA.java
@@ -5,32 +5,31 @@ import java.util.Scanner;
 public class classA {
 
 
-
     @Test
-    public static void classAMethod(){
+    public static void classAMethod() {
         System.out.println("Branch Name: Developer Branch");
         classs();
         classss();
     }
 
-    public static void classs(){
+    public static void classs() {
         System.out.println("Branch Name: Developer Branch");
 
     }
 
-    public static void classss(){
+    public static void classss() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number");
         int num = sc.nextInt();
         reverseNumber(num);
     }
 
-    public static void reverseNumber(int number){
+    public static void reverseNumber(int number) {
         int reverse = 0;
-        while(number!=0){
+        while (number != 0) {
             int digit = number % 10;
             reverse = reverse * 10 + digit;
-            number = number/10;
+            number = number / 10;
         }
         System.out.println("Reversed number " + reverse);
     }
--- untitled/src/test/java/classB.java
+++ untitled/src/test/java/classB.java
@@ -6,21 +6,21 @@ public class classB {
 
     @Test
 
-    public static void classBMethod(){
+    public static void classBMethod() {
 
         boolean flag = false;
         System.out.println("Enter input number");
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        for(int i=2;i<=num/2;i++){
-            if(num%i==0){
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
                 flag = true;
                 break;
             }
         }
-        if(!flag){
+        if (!flag) {
             System.out.println(num + " is a prime number");
-        } else{
+        } else {
             System.out.println(num + " is not a prime number");
         }
     }
