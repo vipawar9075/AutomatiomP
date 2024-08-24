6775f61ff4f864cf8d31bb53686a5decf0fd47b8
Vikas Pawar <vipawar9075@gmail.com>
2024-08-24 14:25:48 +0530

    New method added

--- untitled/src/test/java/classA.java
+++ untitled/src/test/java/classA.java
@@ -5,6 +5,12 @@ public class classA {
     @Test
     public static void classAMethod(){
         System.out.println("Branch Name: Developer Branch");
+        classs();
+    }
+
+    public static void classs(){
+        System.out.println("Branch Name: Developer Branch");
+        
     }
 
 }
