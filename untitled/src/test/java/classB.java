10aa7b029faee74ef86f26e71ad009692e17fc17
Vikas Pawar <vipawar9075@gmail.com>
2024-08-24 16:57:03 +0530

    if else statement class added

---
+++ untitled/src/test/java/classB.java
@@ -0,0 +1,27 @@
+import org.testng.annotations.Test;
+
+import java.util.Scanner;
+
+public class classB {
+
+    @Test
+
+    public static void classBMethod(){
+
+        boolean flag = false;
+        System.out.println("Enter input number");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for(int i=2;i<=num/2;i++){
+            if(num%i==0){
+                flag = true;
+                break;
+            }
+        }
+        if(!flag){
+            System.out.println(num + " is a prime number");
+        } else{
+            System.out.println(num + " is not a prime number");
+        }
+    }
+}
