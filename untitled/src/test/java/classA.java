f4cc01d91c5c5ecc7ecaa88c98d63c285beea483
Vikas Pawar <vipawar9075@gmail.com>
2024-08-24 16:19:52 +0530

    error resolved.

--- untitled/src/test/java/classA.java
+++ untitled/src/test/java/classA.java
@@ -1,18 +1,17 @@
-
+import org.testng.annotations.Test;
 
 public class classA {
+
+
+
+    @Test
     public static void classAMethod(){
-<<<<<<< HEAD
         System.out.println("Branch Name: Developer Branch");
         classs();
     }
-    System.out.println("Branch Name: Developer Branch");
 
     public static void classs(){
         System.out.println("Branch Name: Developer Branch");
-        
-=======
-  System.out.println("Branch Name: Developer Branch");
->>>>>>> f05fcab (Test annotation removed.)
+
     }
 }
