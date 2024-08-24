ae99145c0eba8766dcd10892ac63415a86dfa482
Vikas Pawar <vipawar9075@gmail.com>
2024-08-24 13:42:17 +0530

    Class A added in the developer branch
    # Conflicts:
    #	untitled/src/test/java/classA.java

--- untitled/src/test/java/classA.java
+++ untitled/src/test/java/classA.java
@@ -1,18 +1,10 @@
-
+import org.testng.annotations.Test;
 
 public class classA {
-    public static void classAMethod(){
-<<<<<<< HEAD
-        System.out.println("Branch Name: Developer Branch");
-        classs();
-    }
-    System.out.println("Branch Name: Developer Branch");
 
-    public static void classs(){
-        System.out.println("Branch Name: Developer Branch");
-        
-=======
-  System.out.println("Branch Name: Developer Branch");
->>>>>>> f05fcab (Test annotation removed.)
+    @Test
+    public static void classAMethod(){
+        System.out.println("Branch Name");
     }
+
 }
