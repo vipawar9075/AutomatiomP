88f4f7d61422f1376c962b3b4fabb8051fbe3522
Vikas Pawar <vipawar9075@gmail.com>
2024-08-25 15:19:56 +0530

    TC_01 Updated

--- untitled/src/test/java/TC_01.java
+++ untitled/src/test/java/TC_01.java
@@ -3,6 +3,7 @@ public class TC_01 {
     public static void TC_01Method(){
         System.out.println("TC_01");
         System.out.println("TC_01 After stash");
+        System.out.println("TC_01 After stash");
 
     }
 }
